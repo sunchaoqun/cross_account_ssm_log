a7fe59b1f62cde7471be60d6dfe01bc3c507d7a6
billysun <sunchaoqun@126.com>
2024-04-26 07:11:29 +0000

    fit for @ special word

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,26 @@
 import boto3
 import re
+import urllib.parse
 
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
     # 获取事件中的存储桶和对象键值
     bucket_name = event['Records'][0]['s3']['bucket']['name']
-    object_key = event['Records'][0]['s3']['object']['key']
+    object_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
     
+    print(f"Bucket: {bucket_name}")
+    print(f"Key: {object_key}")
+
+    try:
+        # 尝试获取文件来验证是否存在
+        s3_client.head_object(Bucket=bucket_name, Key=object_key)
+    except s3_client.exceptions.NoSuchKey:
+        print(f"No such key: {object_key}")
+        return f"No such key: {object_key}"
+
     path_parts = object_key.split('/')
     filename_parts = path_parts[-1].split('-')
-    
     original_prefix = filename_parts[0]
     
     # 从S3下载文件
@@ -21,13 +31,8 @@ def lambda_handler(event, context):
     match = re.search(r'i-\w+', file_content)
     if match:
         instance_id = match.group(0)
-        # 组成新文件路径和名称
         new_object_key = f"{path_parts[0]}/{original_prefix}-{instance_id}.log"
-        
-        # 复制对象到新的键值
         s3_client.copy_object(Bucket=bucket_name, CopySource={'Bucket': bucket_name, 'Key': object_key}, Key=new_object_key)
-        
-        # 删除原始对象
         s3_client.delete_object(Bucket=bucket_name, Key=object_key)
         return f"Renamed file to {new_object_key}"
     else:
